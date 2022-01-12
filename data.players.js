de9e76e48e42a3ac7426ae8b2b9d03f4074359d9
Tijsvl <info@tijsvl.net>
2022-01-12 12:42:55 +0200

    GraphQL Demo v1

--- data.players.js
+++ data.players.js
@@ -1,10 +1,10 @@
 // Randomly generated names
 
-const authors = [
+const players = [
   { id: 1, name: 'Cruz, Julius', squadNumber: 2 },
   { id: 2, name: 'Fowler, Erik', squadNumber: 9 },
   { id: 3, name: 'Turner, Rachel', squadNumber: 14 },
   { id: 4, name: 'May, Dominic', squadNumber: 99 }
 ]
 
-module.exports = authors
+module.exports = players
